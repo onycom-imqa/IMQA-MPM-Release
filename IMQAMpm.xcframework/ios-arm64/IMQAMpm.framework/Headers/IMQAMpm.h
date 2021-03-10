da9902da72a2cd9094807b694cc9e9c2a8e751b9
dr-coton <dr.coton@gmail.com>
2021-03-10 12:43:39 +0900

    <release>: Release 3.0.0

---
+++ IMQAMpm.xcframework/ios-arm64/IMQAMpm.framework/Headers/IMQAMpm.h
@@ -0,0 +1,19 @@
+//
+//  IMQAMpm.h
+//  IMQAMpm
+//
+//  Created by Theodore Cha on 2018. 5. 24..
+//  Copyright © 2018년 Theodore Cha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "IMBacktraceHelper.h"
+#import "IMQAReachability.h"
+
+
+//! Project version number for IMQAMpmAgent.
+FOUNDATION_EXPORT double IMQAMpmVersionNumber;
+
+//! Project version string for IMQAMpmAgent.
+FOUNDATION_EXPORT const unsigned char IMQAMpmVersionString[];
+
