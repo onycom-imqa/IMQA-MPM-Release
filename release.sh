7aa7231c0ccdbbe25685a6447dfd1aa5e2c369df
dr-coton <dr.coton@gmail.com>
2021-03-10 12:42:04 +0900

    <Add>: Add release.sh

---
+++ release.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+## 현재 버전 정보 가져오기 
+PRESET_VERSION=$(cat VERSION)
+
+sed -i '' 's|IMQAVERSION|'"${PRESET_VERSION}"'|g' IMQAMpm.podspec
+
+## 현재 버전으로 릴리즈하기 
+git add .
+git commit -m "<release>: Release $PRESET_VERSION"
+
+git tag v$PRESET_VERSION
+git push origin v$PRESET_VERSION
+git push origin master
+
+open https://github.com/onycom-imqa/IMQA-MPM-Release/releases/tag/v$PRESET_VERSION
